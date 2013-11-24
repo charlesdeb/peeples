7808ff3c5e9c1429f1cef29f1619f933151b42c7
charles <chabekah@gmail.com>
2013-11-24 21:21:34 +0200

    finally got add month working with server side calls

--- server/people.js
+++ server/people.js
@@ -1,6 +1,6 @@
 Meteor.methods({
   addNewYearMonth: function(set) {
-    alert('set: ' + set);
+    //alert('set: ' + set);
     People.update({},{$set : set}, {upsert: false, multi: true}, function(error) {
       if (error) {
        // display the error to the user
