c9fc3b499f5ff61f73ae308c3bb355329951603f
charles <chabekah@gmail.com>
2013-11-24 21:23:31 +0200

    move addNewYearMonth method to the client and server.

--- collections/people.js
+++ collections/people.js
@@ -1,1 +1,12 @@
 People = new Meteor.Collection('people');
+
+Meteor.methods({
+  addNewYearMonth: function(set) {
+    People.update({}, {$set: set}, {upsert: false, multi: true}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      }
+    });
+  }
+});
