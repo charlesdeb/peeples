a10662dc5687a842f038a3e9db03af3c3bb94da9
charles <chabekah@gmail.com>
2014-02-09 20:58:25 +0200

    add Month button now works

--- client/views/people/people_header.js
+++ client/views/people/people_header.js
@@ -7,35 +7,36 @@ Template.peopleHeader.helpers({
   hideHideLink: function(yearMonth) {
     return "hide";
   },
-  hideShowLink: function () {
+  hideShowLink: function() {
     if (!hiddenMonths())
       return "hide";
   },
-  months: function () {
+  months: function() {
     return Months.find();
   }
 });
 
-hiddenMonths = function(){
+hiddenMonths = function() {
   if (Months.find({show: false}).count() > 0)
     return true;
 };
 
 Template.peopleHeader.events({
-   'click #add-month a':function(e) {
-     e.preventDefault();
-     largestYearMonth = allYearMonths().pop();
-     if (largestYearMonth === undefined) {
-       today = new Date();
-       largestYearMonth = (today.getFullYear() + "").substring(2,4) + (pad2(today.getMonth() + 1 ));
-     } else {
-       newYearMonth = nextYearMonth(largestYearMonth);
-     }
-     //alert(largestYearMonth);
-     newYearMonth = nextYearMonth(largestYearMonth);
-     set = {};
-     set[newYearMonth] = false;
-     //alert('about to call addYearMonth, set = ' + set.toString());
-     Meteor.call('addNewYearMonth', set);
-   }
+  'click #add-month a': function(e) {
+    e.preventDefault();
+    // largestYearMonth = allYearMonths().pop();
+    largestYearMonth = _.toArray(allYearMonths().collection.docs).pop().month;
+    if (largestYearMonth === undefined) {
+      today = new Date();
+      largestYearMonth = (today.getFullYear() + "").substring(2, 4) + (pad2(today.getMonth() + 1));
+    } else {
+      newYearMonth = nextYearMonth(largestYearMonth);
+    }
+    //alert(largestYearMonth);
+    newYearMonth = nextYearMonth(largestYearMonth);
+    set = {};
+    set[newYearMonth] = false;
+    //alert('about to call addYearMonth, set = ' + set.toString());
+    Meteor.call('addNewYearMonth', set);
+  }
 });
