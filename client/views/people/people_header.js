e9b0c36e0d34447792e5841d1b0abe72de57fe5d
charles <chabekah@gmail.com>
2014-02-22 18:58:32 +0200

    month navigation

--- client/views/people/people_header.js
+++ client/views/people/people_header.js
@@ -1,19 +1,23 @@
 Template.peopleHeader.helpers({
   yearMonthCount: function(yearMonth) {
-    query = {};
+    var query = {};
     var yearMonth_id = Months.findOne({yearMonth: yearMonth})._id
     query['yearMonth_id'] = yearMonth_id;
     return PeopleMonths.find(query).count();
   },
-  hideHideLink: function(yearMonth) {
-    return "hide";
-  },
-  hideShowLink: function() {
-    if (!hiddenMonths())
+  hideDeleteMonth: function(yearMonth) {
+    allYearMonthsArray = _.toArray(allYearMonths().collection.docs);
+    firstYearMonth = _.first(allYearMonthsArray).yearMonth;
+    lastYearMonth = _.last(allYearMonthsArray).yearMonth;
+
+    if (yearMonth == firstYearMonth || yearMonth == lastYearMonth) {
+      // do nothing
+    } else {
       return "hide";
+    }
   },
   months: function() {
-    return Months.find();
+    return allYearMonths();
   }
 });
 
@@ -39,5 +43,30 @@ Template.peopleHeader.events({
     set[newYearMonth] = false;
     //alert('about to call addYearMonth, set = ' + set.toString());
     Meteor.call('addNewYearMonth', set);
+  },
+  'click .delete-month': function(e) {
+    console.log('delete month!');
+
+    var id = $(e.target).closest('a').data('id');
+    console.log('id' + id);
+    Months.remove(id);
+
+//    var name = $(e.target).closest('tr').attr('id');
+//    bootbox.confirm("Are you sure you want to delete '" + name + "'? You can't undo this.", function(result) {
+//      if (result) {
+//        var id = $(e.target).closest('a').data('id');
+//        Meteor.call('deletePerson', id, function(error, id) {
+//          if (error)
+//            return alert(error.reason);
+//        });
+//      }
+//    });
+//
+  },
+  'click .earlier-month': function(e) {
+    console.log('earlier month!');
+  },
+  'click .later-month': function(e) {
+    console.log('later month!');
   }
 });
