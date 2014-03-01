5539b98958265ae73e2a8e08b2d1d389761db2a3
charles <chabekah@gmail.com>
2014-03-01 22:30:52 +0200

    namespace lpad2, reset num of visible months

--- server/fixture.js
+++ server/fixture.js
@@ -1,32 +1,22 @@
 if (People.find().count() === 0) {
   People.insert({
-    name: 'Wallace Footrot' //,
-//    '1312': true,
-//    '1401': true,
-//    '1402': true,
-//    '1403': false
+    name: 'Wallace Footrot'
   });
   People.insert({
-    name: 'Dawg' //,
-//    '1401': true,
-//    '1402': true,
-//    '1403': false
+    name: 'Dawg'
   });
   People.insert({
-    name: 'Cheeky Hobson' //,
-//    '1401': true,
-//    '1402': false,
-//    '1403': true
+    name: 'Cheeky Hobson'
   });
 }
 
 var currentMonth = new Date().getMonth() + 1;
 var currentFullYear = new Date().getFullYear();
-var yearMonth = "" + (currentFullYear - 2000) + pad2(currentMonth);
+var yearMonth = "" + (currentFullYear - 2000) + taco_peeps.pad2(currentMonth);
 // console.log('yearMonth: ' + yearMonth);
 
 var yearMonths = [yearMonth];
-while (yearMonths.length < 15) {
+while (yearMonths.length < taco_peeps.config.visibleMonthCount + 3 ) {
   yearMonth = taco_peeps.nextYearMonth(yearMonth);
   yearMonths.push(yearMonth);
 }
@@ -36,10 +26,10 @@ if (Months.find().count() === 0) {
     return {yearMonth: yearMonth, show: true}
   })
   console.log(yearMonths);
-  console.log(yearMonths[12]);
-  yearMonths[12].show = false;
-  yearMonths[13].show = false;
-  yearMonths[14].show = false;
+  console.log(yearMonths[taco_peeps.config.visibleMonthCount]);
+  yearMonths[taco_peeps.config.visibleMonthCount].show = false;
+  yearMonths[taco_peeps.config.visibleMonthCount + 1].show = false;
+  yearMonths[taco_peeps.config.visibleMonthCount + 2].show = false;
 
   _.each(yearMonths, function(yearMonth) {
     Months.insert(yearMonth)
